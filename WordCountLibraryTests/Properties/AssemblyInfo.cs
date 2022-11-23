d22c21b0288de6c468be5e78de5f1fa2f20f891c
Alex <alex69hu196@gmail.com>
2022-11-23 19:08:54 +0300

    сделана библиотека классов и проделаны тесты

---
+++ WordCountLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordCountLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordCountLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4223fd8b-e962-4f1d-ab1e-7485d4bf8c59")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordCountLibraryTests/WordCountLibraryTestsClass.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WordCountLibrary;
+
+namespace WordCountLibraryTests
+{
+    [TestClass]
+    public class WordCountLibraryTestsClass
+    {
+        /// <summary>
+        /// Ввод пустой строки
+        /// </summary>
+        [TestMethod]
+        public void WordCount_TextAndWordEmpty_ReturnedZero()
+        {
+            //Arrange
+            string text = "";
+            string word = "";
+            int excepted = 0;
+            //Act
+            int actual = WordCountLibraryClass.WordCount(text, word);
+            //Assert
+            Assert.AreEqual(excepted, actual);
+        }
+        /// <summary>
+        /// Вывод кол-ва повторения слова из текста
+        /// </summary>
+        [TestMethod]
+        public void WordCount_WordEmpty_ReturnedWordCount()
+        {
+            //Arrange
+            string text = "Великолепный день сегодня вышел для этого города, который называют пасмурным городом городом";
+            string word = "город";
+            int excepted = 3;
+            //Act
+            int actual = WordCountLibraryClass.WordCount(text, word);
+            //Assert
+            Assert.AreEqual(excepted, actual);
+        }
+        /// <summary>
+        /// Вывод кол-ва повторения слова из текста
+        /// </summary>
+        [TestMethod]
+        public void WordCount_UncorrectWord_ReturnedZero()
+        {
+            //Arrange
+            string text = "Великолепный день сегодня вышел для этого города, который называют пасмурным городом";
+            string word = "динечек";
+            int excepted = 0;
+            //Act
+            int actual = WordCountLibraryClass.WordCount(text, word);
+            //Assert
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
